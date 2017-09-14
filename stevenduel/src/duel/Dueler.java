999320020f71565c4a18650b690bd6234b232a9b
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2017-09-14 11:02:53 -0400

    Changed Dueler and Character B

--- stevenduel/src/duel/Dueler.java
+++ stevenduel/src/duel/Dueler.java
@@ -14,4 +14,8 @@ public interface Dueler {
 	void setStartingHP(int hp);
 	//returns the value of HP, can be called as often as anyone wants
 	int getHP();
+	
+	//This method is called by the Duel and can be used by the dueler
+	//return true if d has hp of given level
+	boolean determineIfOpponentIsFair(Dueler d, int hp);
 }
