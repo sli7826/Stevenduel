12b1329a67651c0cf3dd1f9869111932fe9f0448
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2017-09-14 11:48:23 -0400

    changed stuff

--- stevenduel/src/duel/CharacterB.java
+++ stevenduel/src/duel/CharacterB.java
@@ -2,8 +2,33 @@ package duel;
 
 public class CharacterB implements Dueler
 {
+	private int HP;
+	
 	public CharacterB() 
 	{
-		//asd
+		
+	}
+	public String getName()
+	{
+		return "Bobby";
+	}
+	public void taunt()
+	{
+		System.out.println("insert taunt");
+	}
+	public int getHP()
+	{
+		return HP;
+	}
+	public boolean determineIfOpponentIsFair(Dueler d, int HP) 
+	{
+		if(d.getHP() == HP)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 }
