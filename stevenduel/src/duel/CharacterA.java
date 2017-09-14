6756dad9e6068875425671f0c1140ae4f4ab93d6
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-09-14 11:44:47 -0400

    a

--- stevenduel/src/duel/CharacterA.java
+++ stevenduel/src/duel/CharacterA.java
@@ -1,7 +1,24 @@
 package duel;
 
 public class CharacterA implements Dueler{
-	public CharacterA() {
-		
+	int hp;
+	public CharacterA() {	
+	}
+	public void taunt() {
+		System.out.println("You Suck.");
+	}
+	public String getName() {
+		return "Bob";
+	}
+	public void setStartingHP(int hp) {
+		this.hp=hp;
+	}
+	public int getHP() {
+		return hp;
+	}
+	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
+		if(d.getHP()==hp) 
+			return true;
+		return false;
 	}
 }
